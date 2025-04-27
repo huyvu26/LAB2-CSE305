1730950876db00f34b0eb28a4403f7ab1c7dbedf
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-04-27 16:15:00 +0700

    Update RentalContract.java

--- src/RentalContract.java
+++ src/RentalContract.java
@@ -1,3 +1,33 @@
 public class RentalContract {
+    String contractId;
+    String propertyId;
+    String tenantId;
+    double rentAmount;
 
+    public RentalContract(String contractId, String propertyId, String tenantId, double rentAmount) {
+        this.contractId = contractId;
+        this.propertyId = propertyId;
+        this.tenantId = tenantId;
+        this.rentAmount = rentAmount;
+    }
+
+    public String getContractId() {
+        return contractId;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void createContract() {
+        System.out.println(contractId + propertyId);
+    }
+
+    public void terminateContract() {
+        System.out.println(contractId + " terminate");
+    }
 }
